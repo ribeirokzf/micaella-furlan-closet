0f362791f4725eeb6d185a5cc5b2a6883e65437e
ribeirokzf <nvmreflex@gmail.com>
2024-11-24 20:49:50 -0300

    finalizado??

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Elementos de controle do menu
+    const menuToggle = document.getElementById('menuToggle');
+    const menuAberto = document.querySelector('.menu-aberto');
+
+    // Abre/fecha o menu ao mudar o estado do checkbox
+    menuToggle.addEventListener('change', function () {
+        if (menuToggle.checked) {
+            menuAberto.classList.add('active');
+        } else {
+            menuAberto.classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Elementos de controle do menu
+    const menuToggle = document.getElementById('menuToggle');
+    const menuAberto = document.querySelector('.menu-aberto');
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    const body = document.body;
+
+    // Abre/fecha o menu ao mudar o estado do checkbox
+    menuToggle.addEventListener('change', function () {
+        if (menuToggle.checked) {
+            menuAberto.classList.add('active');
+            body.classList.add('no-scroll');
+        } else {
+            menuAberto.classList.remove('active');
+            body.classList.remove('no-scroll');
+        }
+    });
+
+    // Fecha o menu ao clicar em um link de navegação
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            menuToggle.checked = false;
+            menuAberto.classList.remove('active');
+            body.classList.remove('no-scroll');
+        });
+    });
+});
+
+/* botao back-to-top */
+    var btn =documentSelector("#back-to-top");
+
+    btn.addEventListener("click",function(){
+        window.scrollTo(0,0);
+    });
